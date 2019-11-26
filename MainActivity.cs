4998282cd1338f9ccbbe135cb0bbff90534816ab
Lok Tao Chia <roychia@hotmail.com>
2019-11-26 23:25:51 +0000

    Set up Database Connection

---
+++ MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Firebase;
+using Firebase.Database;
+
+namespace Fitness_Diary
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btntestConnection;
+        FirebaseDatabase database;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btntestConnection = (Button)FindViewById(Resource.Id.mybutton);
+            btntestConnection.Click += BtntestConnection_Click;
+        }
+
+        private void BtntestConnection_Click(object sender, System.EventArgs e)
+        {
+            InitializeDatabase();
+        }
+
+        void InitializeDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+
+            if(app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+
+                .SetApplicationId("fitness-diary-653c2")
+                .SetApiKey("AIzaSyAr8UZlPvW4pDOgvyf434A26rEFxy5t34k")
+                .SetDatabaseUrl("https://fitness-diary-653c2.firebaseio.com")
+                .SetStorageBucket("fitness-diary-653c2.appspot.com")
+                .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            DatabaseReference dbref = database.GetReference("UserSupport");
+            dbref.SetValue("Ticket1");
+
+            Toast.MakeText(this, "Completed", ToastLength.Short).Show();
+        }
+    }
+}
---
+++ obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: eda4e75ae4b73ec3130139a20ebe8dfb032bfe78 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1360
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 356321
+	.include	"typemap.jm.inc"
