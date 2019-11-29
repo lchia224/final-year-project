3d06a73724604357c19a5b605af659b06fff4dfb
Lok Tao Chia <roychia@hotmail.com>
2019-11-29 14:55:24 +0000

    Revert "Added drawer layout"
    This reverts commit 5f953711953e2f4c400fe692e6d62f21b7cc1a5f.
    
    Reverting back before drawer layout

--- MainActivity.cs
+++ MainActivity.cs
@@ -5,48 +5,55 @@ using Android.Runtime;
 using Android.Widget;
 using Firebase;
 using Firebase.Database;
-using Android.Views;
 
 namespace Fitness_Diary
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/FitnessTheme", MainLauncher = false)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
     public class MainActivity : AppCompatActivity
     {
-        Android.Support.V7.Widget.Toolbar mainToolbar;
-        Android.Support.V4.Widget.DrawerLayout drawerLayout;
+        Button btntestConnection;
+        FirebaseDatabase database;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            ConnectControl();
+
+            btntestConnection = (Button)FindViewById(Resource.Id.mybutton);
+            btntestConnection.Click += BtntestConnection_Click;
         }
 
-         void ConnectControl()
+        private void BtntestConnection_Click(object sender, System.EventArgs e)
         {
-            drawerLayout = (Android.Support.V4.Widget.DrawerLayout)FindViewById(Resource.Id.drawerLayout);
+            InitializeDatabase();
+        }
 
-            //setting the toolbar in the main activity page to be transparent
-            mainToolbar = (Android.Support.V7.Widget.Toolbar)FindViewById(Resource.Id.mainToolbar);
-            SetSupportActionBar(mainToolbar);
-            SupportActionBar.Title = null;
-            //replacing toolbar with a mipmap
-            Android.Support.V7.App.ActionBar actionBar = SupportActionBar;
-            actionBar.SetHomeAsUpIndicator(Resource.Mipmap.ic_menu_action);
-            actionBar.SetDisplayHomeAsUpEnabled(true);
-        } 
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
+        void InitializeDatabase()
         {
-            switch(item.ItemId)
+            var app = FirebaseApp.InitializeApp(this);
+
+            if(app == null)
             {
-                case Android.Resource.Id.Home:
-                    drawerLayout.OpenDrawer((int)GravityFlags.Left);
-                    return true;
+                var options = new FirebaseOptions.Builder()
 
-                default:
-                    return base.OnOptionsItemSelected(item);
+                .SetApplicationId("fitness-diary-653c2")
+                .SetApiKey("AIzaSyAr8UZlPvW4pDOgvyf434A26rEFxy5t34k")
+                .SetDatabaseUrl("https://fitness-diary-653c2.firebaseio.com")
+                .SetStorageBucket("fitness-diary-653c2.appspot.com")
+                .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+                database = FirebaseDatabase.GetInstance(app);
             }
-        } 
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            DatabaseReference dbref = database.GetReference("UserSupport");
+            dbref.SetValue("Ticket1");
+
+            Toast.MakeText(this, "Completed", ToastLength.Short).Show();
+        }
     }
 }
