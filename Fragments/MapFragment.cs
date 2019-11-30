98c5d2d08a17775398123848879065132b19c187
Lok Tao Chia <roychia@hotmail.com>
2019-11-30 15:17:34 +0000

    Added Location Permission Request

--- Fragments/MapFragment.cs
+++ Fragments/MapFragment.cs
@@ -11,12 +11,21 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Android.Gms.Maps.Model;
+using Android;
+using Android.Gms.Location.Places.UI;
+using Android.Gms.Location.Places;
+using Android.Gms.Location;
+using Android.Content.PM;
 
 namespace Fitness_Diary.Fragments
 {
     public class MapFragment : Android.Support.V4.App.Fragment, IOnMapReadyCallback
     {
         GoogleMap mainMap;
+
+        readonly string[] permissionGroupLocation = { Manifest.Permission.AccessCoarseLocation, Manifest.Permission.AccessFineLocation };
+        const int requestLocationId = 0;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -30,12 +39,44 @@ namespace Fitness_Diary.Fragments
             SupportMapFragment mapFragment = (SupportMapFragment)ChildFragmentManager.FindFragmentById(Resource.Id.map);
             mapFragment.GetMapAsync(this);
 
+            CheckLocationPermission();
+
             return view;
         }
 
         public void OnMapReady(GoogleMap googleMap)
         {
             mainMap = googleMap;
+        }
+
+        bool CheckLocationPermission()
+        {
+            bool permissionGranted = false;
+
+            if(ActivityCompat.CheckSelfPermission(Activity, Manifest.Permission.AccessFineLocation) != Android.Content.PM.Permission.Granted &&
+                ActivityCompat.CheckSelfPermission(Activity, Manifest.Permission.AccessCoarseLocation) != Android.Content.PM.Permission.Granted)
+            {
+                permissionGranted = false;
+                RequestPermissions(permissionGroupLocation, requestLocationId);
+            }
+            else
+            {
+                permissionGranted = true;
+            }
+
+            return permissionGranted;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            if(grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+            {
+                Toast.MakeText(Activity, "Permission was granted", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(Activity, "Permission was denied", ToastLength.Short).Show();
+            }
         }
     }
 }
