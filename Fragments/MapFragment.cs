d31f0dde29dd1bb92f430f7e80795f323677b003
Lok Tao Chia <roychia@hotmail.com>
2019-12-03 11:02:05 +0000

    Added updates on location changes on map

--- Fragments/MapFragment.cs
+++ Fragments/MapFragment.cs
@@ -17,6 +17,7 @@ using Android.Gms.Location.Places.UI;
 using Android.Gms.Location.Places;
 using Android.Gms.Location;
 using Android.Content.PM;
+using Fitness_Diary.Helpers;
 
 namespace Fitness_Diary.Fragments
 {
@@ -26,6 +27,15 @@ namespace Fitness_Diary.Fragments
 
         readonly string[] permissionGroupLocation = { Manifest.Permission.AccessCoarseLocation, Manifest.Permission.AccessFineLocation };
         const int requestLocationId = 0;
+        LocationRequest mLocationRequest;
+        FusedLocationProviderClient locationClient;
+        Android.Locations.Location mLastLocation;
+        LocationCallbackHelper mLocationCallBack;
+
+        static int UPDATE_INTERVAL = 5; //5 seconds current location updates
+        static int FASTEST_INTERVAL = 5; // 5 seconds current location update
+        static int DISPLACEMENT = 3; //3 meters distance before location gets updated
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -40,15 +50,21 @@ namespace Fitness_Diary.Fragments
             mapFragment.GetMapAsync(this);
 
             CheckLocationPermission();
+            CreateLocationRequest();
+            GetMyLocation();
+            StartLocationUpdates();
 
             return view;
         }
 
         public void OnMapReady(GoogleMap googleMap)
         {
+            bool success = googleMap.SetMapStyle(MapStyleOptions.LoadRawResourceStyle(Activity, Resource.Raw.retromapstyle));
+
             mainMap = googleMap;
         }
 
+        //requesting for location permissions
         bool CheckLocationPermission()
         {
             bool permissionGranted = false;
@@ -67,6 +83,54 @@ namespace Fitness_Diary.Fragments
             return permissionGranted;
         }
 
+        void CreateLocationRequest()
+        {
+            mLocationRequest = new LocationRequest();
+            mLocationRequest.SetInterval(UPDATE_INTERVAL);
+            mLocationRequest.SetFastestInterval(FASTEST_INTERVAL);
+            mLocationRequest.SetPriority(LocationRequest.PriorityHighAccuracy);
+            mLocationRequest.SetSmallestDisplacement(DISPLACEMENT);
+            locationClient = LocationServices.GetFusedLocationProviderClient(Activity);
+            mLocationCallBack = new LocationCallbackHelper();
+            mLocationCallBack.MyLocation += MLocationCallback_MyLocation;
+        }     
+
+        //starts updating user's location
+        void StartLocationUpdates()
+        {
+            if(CheckLocationPermission())
+            {
+                locationClient.RequestLocationUpdates(mLocationRequest, mLocationCallBack, null);
+            }
+        }
+
+        //stops updating user's location
+        void StopLocationUpdates()
+        {
+            if(locationClient != null && mLocationCallBack != null)
+            {
+                locationClient.RemoveLocationUpdates(mLocationCallBack);
+            }
+        }
+
+        //getting location of current position
+        async void GetMyLocation()
+        {
+            if(!CheckLocationPermission())
+            {
+                return;
+            }
+
+            mLastLocation = await locationClient.GetLastLocationAsync();
+            //updates and zooms camera to where the device is current located
+            if(mLastLocation != null)
+            {
+                LatLng myposition = new LatLng(mLastLocation.Latitude, mLastLocation.Longitude);
+                mainMap.MoveCamera(CameraUpdateFactory.NewLatLngZoom(myposition, 17));
+            }
+        }
+
+        //creating message if permissions were accepted or declined
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             if(grantResults[0] == (int)Android.Content.PM.Permission.Granted)
@@ -77,6 +141,14 @@ namespace Fitness_Diary.Fragments
             {
                 Toast.MakeText(Activity, "Permission was denied", ToastLength.Short).Show();
             }
+        }
+
+        //this event will be fired when location changes 
+        void MLocationCallback_MyLocation(object sender, LocationCallbackHelper.OnLocationCapturedEventArgs e)
+        {
+            mLastLocation = e.Location;
+            LatLng myPosition = new LatLng(mLastLocation.Latitude, mLastLocation.Longitude);
+            mainMap.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(myPosition, 17));
         }
     }
 }
---
+++ Helpers/LocationCallbackHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Location;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Fitness_Diary.Helpers
+{
+    class LocationCallbackHelper : LocationCallback
+    {
+        public EventHandler<OnLocationCapturedEventArgs> MyLocation;
+        public class OnLocationCapturedEventArgs : EventArgs
+        {
+            public Android.Locations.Location Location { get; set; }
+        }
+
+        public override void OnLocationAvailability(LocationAvailability locationAvailability)
+        {
+            //logging if user has allowed location permission
+            Log.Debug("Fitness Diary", "IsLocationAvailable: {0}", locationAvailability.IsLocationAvailable);
+        }
+        
+        //location updates on current location
+        public override void OnLocationResult(LocationResult result)
+        {
+            if(result.Locations.Count != 0)
+            {
+                MyLocation?.Invoke(this, new OnLocationCapturedEventArgs
+                {
+                    Location = result.Locations[0]
+                });
+            }
+        }
+    }
+}
